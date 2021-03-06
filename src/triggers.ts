f1f44b8251cf6510cf5405e6abd0f2f3e6fd7ee2
Oleg Valter <o.a.philippov@gmail.com>
2021-03-07 00:32:34 +0300

    Reworked triggers, added getter/setter for missing analytics info

--- src/triggers.ts
+++ src/triggers.ts
@@ -1,3 +1,5 @@
+/// <reference path="../triggers.d.ts" />
+
 const getDailyClearConfig = () => {
   const defaultConfig = JSON.stringify({ days: 1, atMinute: 0, atHour: 0 });
   return JSON.parse(getProperty(APP_CONFIG.properties.clear, defaultConfig));
@@ -11,43 +13,75 @@ declare interface DailyClearOptions {
 
 const setDailyClearConfig = (config: DailyClearOptions) => {
   const { days = 1, atMinute = 0, atHour = 0 } = config;
-  return setProperty(
-    APP_CONFIG.properties.clear,
-    JSON.stringify({ days, atMinute, atHour })
-  );
+  return setProperty(APP_CONFIG.properties.clear, { days, atMinute, atHour });
 };
 
-const handleDailyClear = ({ 
-  onError = console.warn 
-} = {}) => {
+const handleDailyClear = ({ onError = console.warn } = {}) => {
   try {
     clearSheet({
+      onError,
+      skipRows: [1],
       name: sSHEET_FORM,
       type: "values",
     });
     return true;
   } catch (error) {
-    onError(error);
+    onError(`failed to clear: ${error}`);
     return false;
   }
 };
 
 /**
+ * @summary reusable utility preparing project triggers
+ */
+const prepareTriggersForUse = () => {
+  const onInstallFailure = (msg: string) =>
+    console.warn(`failed to install trigger: ${msg}`);
+
+  const tracking = TriggersApp.trackTriggers();
+
+  if (!tracking) {
+    onInstallFailure("failed to track triggers");
+    return false;
+  }
+
+  const common = { unique: true, onInstallFailure };
+
+  const submitInstalled = TriggersApp.getOrInstallTrigger({
+    ...common,
+    callbackName: "onFormSubmit", //@ts-ignore
+    type: TriggersApp.TriggerTypes.SUBMIT,
+  });
+
+  const editInstalled = TriggersApp.getOrInstallTrigger({
+    ...common,
+    callbackName: "onEditEvent", //@ts-ignore
+    type: TriggersApp.TriggerTypes.EDIT,
+  });
+
+  const clearInstalled = TriggersApp.getOrInstallTrigger({
+    ...common,
+    installerConfig: getDailyClearConfig(),
+    callbackName: "handleDailyClear", //@ts-ignore
+    type: TriggersApp.TriggerTypes.CLOCK,
+  });
+
+  return tracking && submitInstalled && editInstalled && !!clearInstalled;
+};
+
+/**
  * @summary reschedules clear trigger
- * @returns {boolean}
  */
 const rescheduleClearTrigger = ({
   atHour = 0,
   atMinute = 1,
   days = 1,
-}: DailyClearOptions = {}) => {
+}: DailyClearOptions = {}): boolean => {
   const funcName = "handleDailyClear";
 
-  // @ts-ignore
   const info = TriggersApp.getTrackedTriggerInfo({ funcName });
 
-  // @ts-ignore
-  const status = TriggersApp.deleteTracked(info);
+  const status = !info || TriggersApp.deleteTracked(info);
 
   if (!status) {
     return false;
@@ -59,14 +93,79 @@ const rescheduleClearTrigger = ({
     return false; //TODO: use different status codes;
   }
 
-  // @ts-ignore
   const installed = TriggersApp.getOrInstallTrigger({
     unique: true,
     installerConfig: { atHour, atMinute, days },
-    callbackName: funcName
+    callbackName: funcName,
   });
 
   console.log({ atHour, atMinute, info, status, settingsStatus, installed });
 
   return !!installed;
 };
+
+const makeNoGAstatus = (): NoAnalyticsStatus => ({
+  status: false,
+  dismissed: false,
+});
+
+/**
+ * @summary gets metadata that install of UA is dismissed
+ */
+const getGaInstalledDismissed = (sheet = getFormSheet()) => {
+  const {
+    properties: { metadata: key },
+  } = APP_CONFIG;
+
+  if (!sheet) throw new RangeError(`form sheet not found`);
+
+  const analyticsStatus: NoAnalyticsStatus = getMetadataValue({
+    sheet,
+    key,
+    def: makeNoGAstatus(),
+  });
+
+  return analyticsStatus;
+};
+
+/**
+ * @summary sets metadata that install of UA is dismissed
+ */
+const setGaInstallDismissed = (sheet = getFormSheet()) => {
+  const {
+    properties: { metadata: key },
+  } = APP_CONFIG;
+
+  if (!sheet) throw new RangeError(`form sheet not found`);
+
+  const analyticsStatus: NoAnalyticsStatus = getMetadataValue({
+    sheet,
+    key,
+    def: makeNoGAstatus(),
+  });
+
+  analyticsStatus.dismissed = true;
+
+  return setMetadataValue({ sheet, key, value: analyticsStatus });
+};
+
+/**
+ * @summary checks if the user has GA installed and prompts accordingly
+ */
+function promptUAinstall() {
+  const { dismissed, status } = getGaInstalledDismissed();
+
+  console.log(status, dismissed);
+
+  if (!status || dismissed) {
+    return;
+  }
+
+  const ui = SpreadsheetApp.getUi();
+  const content = loadTemplate("html", "installGA", {
+    style: "html",
+    run: "html",
+  });
+  content.setHeight(175);
+  ui.showModalDialog(content, "Warning");
+}
