34fb31b5396903d796a6dfd2a647b8ded5df7e9c
Oleg Valter <o.a.philippov@gmail.com>
2021-04-03 01:26:44 +0300

    Added day offset util

--- src/utils/dateHelpers.ts
+++ src/utils/dateHelpers.ts
@@ -8,3 +8,9 @@ const toSparseISO8601 = (date = new Date()) =>
     Session.getScriptTimeZone(),
     "yyyy-MM-dd hh:mm:ssXXX"
   ) as SparseISO8601DTstring;
+
+const offsetDays = (date: Date, days = 0) => {
+  const copy = new Date(date.getTime());
+  copy.setDate(copy.getDate() + days);
+  return copy;
+};
