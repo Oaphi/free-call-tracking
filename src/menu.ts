0a0ba229f3bd5c9168e7b53940f9bbd59bb68068
Oleg Valter <o.a.philippov@gmail.com>
2021-06-17 15:53:19 +0300

    added first-time use instruction loader

--- src/menu.ts
+++ src/menu.ts
@@ -1,89 +1,113 @@
 function onInstall(e: GoogleAppsScript.Events.AddonOnInstall) {
-  recordNewOwner();
-  return onOpen(e);
+    recordNewOwner();
+    return onOpen(e);
 }
+
+/**
+ * @summary shows instructions on first-time use
+ */
+const showInstructionsOnFirstTime = () => {
+    const templ = loadTemplate(false, "html", "firstTimePrompt.html", {
+        utils: "html",
+        style: "html",
+        run: "html",
+    });
+
+    const content = template({
+        template: templ,
+        vars: {
+            tid: APP_CONFIG.ids.analytics,
+            page_path: "/first_time",
+            page_title: "first_time",
+            page_url: getActiveURL(),
+        },
+    });
+
+    const ui = SpreadsheetApp.getUi();
+
+    const output = HtmlService.createHtmlOutput(content);
+    return ui.showModalDialog(output, "New Version First Time Use");
+};
 
 /**
  * @summary trigger firing on spreadsheet open
  */
 function onOpen({
-  authMode = ScriptApp.AuthMode.FULL,
+    authMode = ScriptApp.AuthMode.FULL,
 }:
-  | GoogleAppsScript.Events.SheetsOnOpen
-  | GoogleAppsScript.Events.AddonOnInstall) {
-  //can work with triggers
-  if (authMode !== ScriptApp.AuthMode.NONE) {
-    prepareTriggersForUse();
-  }
+    | GoogleAppsScript.Events.SheetsOnOpen
+    | GoogleAppsScript.Events.AddonOnInstall) {
+    //can work with triggers
+    if (authMode !== ScriptApp.AuthMode.NONE) prepareTriggersForUse();
 
-  const {
-    menus: {
-      main: { items },
-    },
-  } = getConfig();
+    const {
+        menus: {
+            main: { items },
+        },
+    } = getConfig();
 
-  SpreadsheetApp.getUi()
-    .createAddonMenu()
-    .addItem(items.deploy, deployAddonGo.name)
-    .addItem(items.installUA, promptUAinstall.name)
-    .addItem(items.openSettings, settingsGo.name)
-    .addItem(items.openHelp, helpGo.name)
-    .addToUi();
+    SpreadsheetApp.getUi()
+        .createAddonMenu()
+        .addItem(items.deploy, deployAddonGo.name)
+        .addItem(items.installUA, promptUAinstall.name)
+        .addItem(items.openSettings, settingsGo.name)
+        .addItem(items.openHelp, helpGo.name)
+        .addToUi();
 }
 
 const getActiveURL = () => SpreadsheetApp.getActiveSpreadsheet().getUrl();
 
 const sidebarFromString = (content: string) =>
-  SpreadsheetApp.getUi().showSidebar(HtmlService.createHtmlOutput(content));
+    SpreadsheetApp.getUi().showSidebar(HtmlService.createHtmlOutput(content));
 
 /**
  * @summary triggers settings screen sidebar
  */
 function settingsGo() {
-  prepareTriggersForUse();
+    prepareTriggersForUse();
 
-  recordNewOwner();
+    recordNewOwner();
 
-  const templ = loadTemplate(false, "html", "settings.html", {
-    utils: "html",
-    style: "html",
-    run: "html",
-  });
+    const templ = loadTemplate(false, "html", "settings.html", {
+        utils: "html",
+        style: "html",
+        run: "html",
+    });
 
-  const content = template({
-    template: templ,
-    vars: {
-      tid: APP_CONFIG.ids.analytics,
-      page_path: "/settings",
-      page_title: "settings",
-      page_url: getActiveURL(),
-    },
-  });
+    const content = template({
+        template: templ,
+        vars: {
+            tid: APP_CONFIG.ids.analytics,
+            page_path: "/settings",
+            page_title: "settings",
+            page_url: getActiveURL(),
+        },
+    });
 
-  return sidebarFromString(content);
+    return sidebarFromString(content);
 }
 
 /**
  * @summary triggers help screen sidebar
  */
 function helpGo() {
-  recordNewOwner();
+    recordNewOwner();
 
-  const templ = loadTemplate(false, "html", "help.html", {
-    utils: "html",
-    style: "html",
-    run: "html",
-  });
+    const templ = loadTemplate(false, "html", "help.html", {
+        utils: "html",
+        style: "html",
+        run: "html",
+    });
 
-  const content = template({
-    template: templ,
-    vars: {
-      tid: APP_CONFIG.ids.analytics,
-      page_path: "/help",
-      page_title: "help",
-      page_url: getActiveURL(),
-    },
-  });
+    const content = template({
+        template: templ,
+        vars: {
+            tid: APP_CONFIG.ids.analytics,
+            page_path: "/help",
+            page_title: "help",
+            page_url: getActiveURL(),
+        },
+    });
 
-  return sidebarFromString(content);
+    return sidebarFromString(content);
 }
